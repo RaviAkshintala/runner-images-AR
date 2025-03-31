29190db3e2ce983dcd27047a362885da92ee886e
Ravi Akshintala <raviakshintalagithub.com@Ravis-Laptop.local>
2025-03-31 16:37:48 +0530

    [Windows-25] updated the azure cosmos db emulator thumbprint signature

--- images/windows/scripts/build/Install-AzureCosmosDbEmulator.ps1
+++ images/windows/scripts/build/Install-AzureCosmosDbEmulator.ps1
@@ -5,6 +5,6 @@
 
 Install-Binary -Type MSI `
     -Url "https://aka.ms/cosmosdb-emulator" `
-    -ExpectedSHA256Sum "1597C1809DCF8E3270E7B1972B05EEFED4055B8BE9B33BA17B72AAD799DE70F5"
+    -ExpectedSHA256Sum "1EF64CF9CD13C611AAC394113A703AF6F525C7C39BFB6B4C3F884D57F6F1D1CF"
 
 Invoke-PesterTests -TestFile "Tools" -TestName "Azure Cosmos DB Emulator"
