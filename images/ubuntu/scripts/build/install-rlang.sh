9adaf72b4afa528ba3ff6f777254002eb7bf97c8
Ravi Akshintala <raviakshintala@github.com>
2025-04-15 00:28:38 +0530

    [Ubuntu 22] pinning the previous version of alibaba and updated the rlang script

--- images/ubuntu/scripts/build/install-rlang.sh
+++ images/ubuntu/scripts/build/install-rlang.sh
@@ -11,7 +11,7 @@ wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | gpg
 echo "deb [signed-by=/usr/share/keyrings/rlang.gpg] https://cloud.r-project.org/bin/linux/ubuntu $os_label-cran40/" > /etc/apt/sources.list.d/rlang.list
 
 apt-get update
-apt install --no-install-recommends r-base
+apt-get install --no-install-recommends r-base
 
 rm /etc/apt/sources.list.d/rlang.list
 rm /usr/share/keyrings/rlang.gpg
