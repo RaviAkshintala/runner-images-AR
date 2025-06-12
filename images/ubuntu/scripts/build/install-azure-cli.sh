081f0edee46c044f0780b12db8dfaa2d417d885c
Ravi Akshintala <raviakshintala@github.com>
2025-06-12 13:01:01 +0530

    [Ubuntu] pinning the previous of the azure cli

--- images/ubuntu/scripts/build/install-azure-cli.sh
+++ images/ubuntu/scripts/build/install-azure-cli.sh
@@ -5,7 +5,11 @@
 ################################################################################
 
 # Install Azure CLI (instructions taken from https://docs.microsoft.com/en-us/cli/azure/install-azure-cli)
-curl -fsSL https://aka.ms/InstallAzureCLIDeb | sudo bash
+#Piining the specific version of the Azure CLI
+# Obtain the currently installed distribution
+AZ_DIST=$(lsb_release -cs)
+AZ_VER=2.73.0
+sudo apt-get install azure-cli=${AZ_VER}-1~${AZ_DIST}
 
 echo "azure-cli https://docs.microsoft.com/en-us/cli/azure/install-azure-cli-linux?pivots=apt" >> $HELPER_SCRIPTS/apt-sources.txt
 
