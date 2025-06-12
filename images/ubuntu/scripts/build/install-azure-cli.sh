ae6fee2e0d14a1b508d16729d1c670cbfc2978c0
Ravi Akshintala <raviakshintala@github.com>
2025-06-12 13:11:39 +0530

    [Ubuntu] pinning the speicifc version of Azure CLI

--- images/ubuntu/scripts/build/install-azure-cli.sh
+++ images/ubuntu/scripts/build/install-azure-cli.sh
@@ -5,7 +5,33 @@
 ################################################################################
 
 # Install Azure CLI (instructions taken from https://docs.microsoft.com/en-us/cli/azure/install-azure-cli)
-curl -fsSL https://aka.ms/InstallAzureCLIDeb | sudo bash
+# Pinning the specific version of Azure CLI due to issue in the latet version 
+# Get packages needed for the installation
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release
+# Download and install the Microsoft signing key:
+sudo mkdir -p /etc/apt/keyrings
+curl -sLS https://packages.microsoft.com/keys/microsoft.asc |
+gpg --dearmor | sudo tee /etc/apt/keyrings/microsoft.gpg > /dev/null
+sudo chmod go+r /etc/apt/keyrings/microsoft.gpg
+# Add the Azure CLI software repository:
+AZ_DIST=$(lsb_release -cs)
+echo "Types: deb
+URIs: https://packages.microsoft.com/repos/azure-cli/
+Suites: ${AZ_DIST}
+Components: main
+Architectures: $(dpkg --print-architecture)
+Signed-by: /etc/apt/keyrings/microsoft.gpg" | sudo tee /etc/apt/sources.list.d/azure-cli.sources
+# Install the specific version
+sudo apt-get update
+# Obtain the currently installed distribution
+AZ_DIST=$(lsb_release -cs)
+
+# Store an Azure CLI version of choice
+AZ_VER=2.51.0
+
+# Install a specific version
+sudo apt-get install azure-cli=${AZ_VER}-1~${AZ_DIST}
 
 echo "azure-cli https://docs.microsoft.com/en-us/cli/azure/install-azure-cli-linux?pivots=apt" >> $HELPER_SCRIPTS/apt-sources.txt
 
