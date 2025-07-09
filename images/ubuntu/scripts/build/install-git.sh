db3a60cd488fada295380f205725a0e157889c26
Ravi Akshintala <raviakshintala@github.com>
2025-07-09 15:43:34 +0530

    [Ubuntu] changing the installation method of Git to get 2.50.1 version

--- images/ubuntu/scripts/build/install-git.sh
+++ images/ubuntu/scripts/build/install-git.sh
@@ -30,8 +30,7 @@ sudo apt-get install -y git-ftp
 # Clean up the tarball
 rm -f git-2.50.1.tar.gz
 
-# Document the installed Git version and dependencies
-git --version
+
 
 # Add SSH keys for known hosts
 echo "Adding GitHub and Azure SSH keys to known_hosts..."
